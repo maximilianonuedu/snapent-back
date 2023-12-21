0e911156be1f87921113f985277f8a5e102eafb2
maximilianonuedu <maximiliano.nunez@davinci.edu.ar>
2023-12-21 18:02:03 -0300

    Ajuste db conect

--- global/conexion_db.js
+++ global/conexion_db.js
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient('mongodb+srv://mailengomez:snapent123@cluster0.6ddjcic.mongodb.net/test')
+const client = new MongoClient('mongodb+srv://mailengomez:snapent123@cluster0.6ddjcic.mongodb.net/?retryWrites=true&w=majority')
+
 
 await client.connect()
 
